a73428d32c9542f68a13fb3e3e746a18144533d2
dolansete <dolancc00@gmail.com>
2022-03-28 11:53:49 +0200

    Si que va, estaba haciendo peticiones en raw en vez de json

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,15 @@
-const res = require("express/lib/response");
 const mongoose = require("mongoose");
 
 const User = mongoose.model("User");
 
-module.exports.register = (req, resp, next) => {
-  console.log("Registrame esta");
+module.exports.register = (req, res, next) => {
+  console.log("Registrando");
   var user = new User();
   user.username = req.body.username;
   user.email = req.body.email;
   user.password = req.body.password;
   user.save((err, doc) => {
     console.log(doc);
-    if (!err) resp.send(doc);
+    if (!err) res.send(doc);
   });
 };
