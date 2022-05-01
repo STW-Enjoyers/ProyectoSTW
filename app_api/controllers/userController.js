45279bf43f7a5bd3ad9d3b7af191c278057f3df7
dolansete <dolancc00@gmail.com>
2022-05-01 12:50:46 +0200

    Username unico

--- app_api/controllers/userController.js
+++ app_api/controllers/userController.js
@@ -2,25 +2,31 @@ const passport = require("passport");
 const _ = require("lodash");
 const User = require("../models/userSchema");
 const { urlencoded } = require("body-parser");
+const logger = require("../../logger");
 
 const register = function (req, res, next) {
   logger.info("Registrando");
-  var user = new User();
-  user.username = req.body.username;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.save((err, doc) => {
-    if (!err) {
-      passport.authenticate("local", (err, user, info) => {
-        if (err) return res.status(400).json(err);
-        else if (user)
-          return res.status(200).json({ token: user.jwtGen(), doc });
-        else return res.status(404).json(info);
-      })(req, res);
-    } else {
-      if (err.code == 11000)
-        res.status(422).send(["Duplicate email address found."]);
-      else return next(err);
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (user) res.status(422).send(["Duplicate username found."]);
+    else {
+      var user = new User();
+      user.username = req.body.username;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.save((err, doc) => {
+        if (!err) {
+          passport.authenticate("local", (err, user, info) => {
+            if (err) return res.status(400).json(err);
+            else if (user)
+              return res.status(200).json({ token: user.jwtGen(), doc });
+            else return res.status(404).json(info);
+          })(req, res);
+        } else {
+          if (err.code == 11000)
+            res.status(422).send(["Duplicate email address found."]);
+          else return next(err);
+        }
+      });
     }
   });
 };
@@ -40,9 +46,10 @@ const profile = function (req, res, next) {
         .status(404)
         .json({ status: false, message: "No se encontró el usuario :C" });
     else
-      return res
-        .status(200)
-        .json({ status: true, user: _.pick(user, ["username", "email"]) });
+      return res.status(200).json({
+        status: true,
+        user: _.pick(user, ["_id", "username", "email"]),
+      });
   });
 };
 
